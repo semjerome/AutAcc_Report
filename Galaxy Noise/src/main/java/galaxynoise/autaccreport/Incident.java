ea157c00a349e4e6bc8bf9df829ab0249c28b174
semjeromers <semjeromework@gmail.com>
2016-11-07 21:50:52 -0500

    Halfway done on Incident class.

--- Galaxy Noise/src/main/java/galaxynoise/autaccreport/Incident.java
+++ Galaxy Noise/src/main/java/galaxynoise/autaccreport/Incident.java
@@ -1,9 +1,23 @@
 package galaxynoise.autaccreport;
 
+import java.util.Date;
+
 /**
  * Created by semjeromers on 11/7/2016.
  * Class object for Incident
  */
 
 public class Incident {
+
+    String insuranceNumber;
+    Date incidentDate;
+    int longli;
+    int lati;
+    int reportId;
+
+
+    public Incident()
+    {
+
+    }
 }
