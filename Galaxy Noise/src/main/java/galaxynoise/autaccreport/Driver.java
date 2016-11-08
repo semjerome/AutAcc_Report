cb0a13d751081a59647669211360dbd06847675d
semjeromers <semjeromework@gmail.com>
2016-11-07 20:17:44 -0500

    Complete Driver object

--- Galaxy Noise/src/main/java/galaxynoise/autaccreport/Driver.java
+++ Galaxy Noise/src/main/java/galaxynoise/autaccreport/Driver.java
@@ -6,4 +6,57 @@ package galaxynoise.autaccreport;
  */
 
 public class Driver {
+    String driverLicense;
+    String firstName;
+    String lastName;
+    String gender;
+
+    public Driver()
+    {
+
+    }
+    public Driver(String driverLicense, String firstName, String lastName, String gender)
+    {
+        driverLicense=driverLicense;
+        firstName=firstName;
+        lastName=lastName;
+        gender=gender;
+    }
+
+    public String getDriverLicense() {
+        return driverLicense;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setDriverLicense(String driverLicense) {
+        this.driverLicense = driverLicense;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
