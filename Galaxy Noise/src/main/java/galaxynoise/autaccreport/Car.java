571c735c28c12240aee82caf2ebad3bd9d570b2c
semjeromers <semjeromework@gmail.com>
2016-11-07 20:24:44 -0500

    Complete Driver object

--- Galaxy Noise/src/main/java/galaxynoise/autaccreport/Car.java
+++ Galaxy Noise/src/main/java/galaxynoise/autaccreport/Car.java
@@ -6,4 +6,53 @@ package galaxynoise.autaccreport;
  */
 
 public class Car {
+
+    String plateNumber;
+    String carMake;
+    String carModel;
+    int carYEar;
+
+    public Car()
+    {
+
+    }
+    public Car(String plateNumber, String carMake, String carModel, int carYEar)
+    {
+       plateNumber=plateNumber;
+        carMake=carMake;
+        carModel=carModel;
+        carYEar=carYEar;
+    }
+
+    public int getCarYEar() {
+        return carYEar;
+    }
+
+    public String getCarMake() {
+        return carMake;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setCarMake(String carMake) {
+        this.carMake = carMake;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public void setCarYEar(int carYEar) {
+        this.carYEar = carYEar;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
 }
