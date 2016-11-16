6015e1d1bddb211804cf8c1a3b34a1f3e548f029
n01086924 <myuz97@naver.com>
2016-11-16 14:51:39 -0500

    .

--- Galaxy Noise/src/main/java/galaxynoise/autaccreport/MainActivity.java
+++ Galaxy Noise/src/main/java/galaxynoise/autaccreport/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button b1 = (Button) findViewById(R.id.button);
 
+
         /*
         * In this part, we will add the function which can login sql and
         * check if it is available to connect to sql
