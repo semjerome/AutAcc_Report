dfb0d85268e6794c217acf1d9bdb127f7a478675
n01086924 <myuz97@naver.com>
2016-11-15 20:22:50 -0500

    Should modify second activity

---
+++ Galaxy Noise/src/main/java/galaxynoise/autaccreport/MainActivity.java
@@ -0,0 +1,33 @@
+package galaxynoise.autaccreport;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1 = (Button) findViewById(R.id.button);
+
+        /*
+        * In this part, we will add the function which can login sql and
+        * check if it is available to connect to sql
+        * If it can't, ask to enter ID & password again
+        * Otherwise, it will go to searching activity
+        * */
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v)
+            {
+                Intent intent = new Intent(getApplicationContext(), searching.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
